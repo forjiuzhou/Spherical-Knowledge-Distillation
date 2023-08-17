40142b5ea66f40c269f9bf87ecba42f7a123061a
郭嘉 <zuobo@xiaohongshu.com>
2023-08-17 20:04:20 +0800

    test

--- skd_main.py
+++ skd_main.py
@@ -93,10 +93,34 @@ cudnn.benchmark = True
 
 
 class HybridTrainPipe(Pipeline):
-    def __init__(self, batch_size, data_dir, num_threads=4, device_id=0, crop=224, dali_cpu=False):
+    def __init__(self, batch_size, num_threads, device_id, data_dir, crop, dali_cpu=False):
         super(HybridTrainPipe, self).__init__(batch_size, num_threads, device_id, seed=12 + device_id)
-        self.input = ops.FileReader(file_root=data_dir, random_shuffle=True)
+        # self.input = ops.FileReader(file_root=data_dir, shard_id=args.local_rank, num_shards=args.world_size, random_shuffle=True)
+        index_path = []
+        for path in os.listdir("/home/yz979/code/imagenet/ImageNet-to-TFrecord/idx_files"):
+            index_path.append(os.path.join("/home/yz979/code/imagenet/ImageNet-to-TFrecord/idx_files", path))
+        index_path = sorted(index_path)
+        self.input = ops.TFRecordReader(path=data_dir, index_path=index_path, shard_id=args.local_rank,
+                                        num_shards=args.world_size, random_shuffle=True,
+                                        features={
+                                                    'image/height': tfrec.FixedLenFeature([1], tfrec.int64,  -1),
+                                                    'image/width': tfrec.FixedLenFeature([1], tfrec.int64,  -1),
+                                                    'image/colorspace': tfrec.FixedLenFeature([ ], tfrec.string, ''),
+                                                    'image/channels': tfrec.FixedLenFeature([], tfrec.int64,  -1),
+                                                    'image/class/label': tfrec.FixedLenFeature([1], tfrec.int64,  -1),
+                                                    'image/class/synset': tfrec.FixedLenFeature([ ], tfrec.string, ''),
+                                                    # 'image/class/text': tfrec.FixedLenFeature([ ], tfrec.string, ''),
+                                                    # 'image/object/bbox/xmin': tfrec.VarLenFeature(tfrec.float32, 0.0),
+                                                    # 'image/object/bbox/xmax': tfrec.VarLenFeature(tfrec.float32, 0.0),
+                                                    # 'image/object/bbox/ymin': tfrec.VarLenFeature(tfrec.float32, 0.0),
+                                                    # 'image/object/bbox/ymax': tfrec.VarLenFeature(tfrec.float32, 0.0),
+                                                    # 'image/object/bbox/label': tfrec.FixedLenFeature([1], tfrec.int64,-1),
+                                                    'image/format': tfrec.FixedLenFeature((), tfrec.string, ""),
+                                                    'image/filename': tfrec.FixedLenFeature((), tfrec.string, ""),
+                                                    'image/encoded': tfrec.FixedLenFeature((), tfrec.string, "")
+                                                })
 
+        #let user decide which pipeline works him bets for RN version he runs
         dali_device = 'cpu' if dali_cpu else 'gpu'
         decoder_device = 'cpu' if dali_cpu else 'mixed'
         # This padding sets the size of the internal nvJPEG buffers to be able to handle all images from full-sized ImageNet
@@ -107,7 +131,7 @@ class HybridTrainPipe(Pipeline):
                                                  device_memory_padding=device_memory_padding,
                                                  host_memory_padding=host_memory_padding,
                                                  random_aspect_ratio=[0.8, 1.25],
-                                                 random_area=[0.6, 1.0],
+                                                 random_area=[0.1, 1.0],
                                                  num_attempts=100)
         self.res = ops.Resize(device=dali_device, resize_x=crop, resize_y=crop, interp_type=types.INTERP_TRIANGULAR)
         self.cmnp = ops.CropMirrorNormalize(device="gpu",
@@ -118,19 +142,40 @@ class HybridTrainPipe(Pipeline):
                                             mean=[0.485 * 255,0.456 * 255,0.406 * 255],
                                             std=[0.229 * 255,0.224 * 255,0.225 * 255])
         self.coin = ops.CoinFlip(probability=0.5)
+        print('DALI "{0}" variant'.format(dali_device))
 
     def define_graph(self):
         rng = self.coin()
-        self.jpegs, self.labels = self.input(name="Reader")
+        inputs = self.input()
+        self.jpegs = inputs["image/encoded"]
+        self.labels = inputs["image/class/label"]
         images = self.decode(self.jpegs)
         images = self.res(images)
         output = self.cmnp(images.gpu(), mirror=rng)
         return [output, self.labels]
 
+
 class HybridValPipe(Pipeline):
-    def __init__(self, batch_size, data_dir, num_threads=4, device_id=0, crop=224, size=224):
+    def __init__(self, batch_size, num_threads, device_id, data_dir, crop, size):
         super(HybridValPipe, self).__init__(batch_size, num_threads, device_id, seed=12 + device_id)
-        self.input = ops.FileReader(file_root=data_dir, random_shuffle=False)
+        # self.input = ops.FileReader(file_root=data_dir, shard_id=args.local_rank, num_shards=args.world_size, random_shuffle=False)
+        index_path = []
+        for path in os.listdir("/home/yz979/code/imagenet/ImageNet-to-TFrecord/val_idx_files"):
+            index_path.append(os.path.join("/home/yz979/code/imagenet/ImageNet-to-TFrecord/val_idx_files", path))
+        index_path = sorted(index_path)
+        self.input = ops.TFRecordReader(path=data_dir, index_path=index_path, shard_id=args.local_rank,
+                                        num_shards=args.world_size, random_shuffle=True,
+                                        features={
+                                            'image/height': tfrec.FixedLenFeature([1], tfrec.int64, -1),
+                                            'image/width': tfrec.FixedLenFeature([1], tfrec.int64, -1),
+                                            'image/colorspace': tfrec.FixedLenFeature([], tfrec.string, ''),
+                                            'image/channels': tfrec.FixedLenFeature([], tfrec.int64, -1),
+                                            'image/class/label': tfrec.FixedLenFeature([1], tfrec.int64, -1),
+                                            'image/class/synset': tfrec.FixedLenFeature([], tfrec.string, ''),
+                                            'image/format': tfrec.FixedLenFeature((), tfrec.string, ""),
+                                            'image/filename': tfrec.FixedLenFeature((), tfrec.string, ""),
+                                            'image/encoded': tfrec.FixedLenFeature((), tfrec.string, "")
+                                        })
         self.decode = ops.ImageDecoder(device="mixed", output_type=types.RGB)
         self.res = ops.Resize(device="gpu", resize_shorter=size, interp_type=types.INTERP_TRIANGULAR)
         self.cmnp = ops.CropMirrorNormalize(device="gpu",
@@ -142,7 +187,12 @@ class HybridValPipe(Pipeline):
                                             std=[0.229 * 255,0.224 * 255,0.225 * 255])
 
     def define_graph(self):
-        self.jpegs, self.labels = self.input(name="Reader")
+        # self.jpegs, self.labels = self.input(name="Reader")
+
+        inputs = self.input()
+        self.jpegs = inputs["image/encoded"]
+        self.labels = inputs["image/class/label"]
+
         images = self.decode(self.jpegs)
         images = self.res(images)
         output = self.cmnp(images)
@@ -266,8 +316,8 @@ def main():
         for path in os.listdir(os.path.join(args.data[0], "train")):
             traindir.append(os.path.join(args.data[0],"train", path))
         traindir = sorted(traindir)
-        for path in os.listdir(os.path.join(args.data[0], "val")):
-            valdir.append(os.path.join(args.data[0], "val",path))
+        for path in os.listdir(os.path.join(args.data[0], "validation")):
+            valdir.append(os.path.join(args.data[0], "validation",path))
         valdir = sorted(valdir)
         print(len(valdir), len(traindir))
     else:
@@ -404,6 +454,9 @@ def train_kd(train_loader, model, teacher, criterion, optimizer, epoch):
             #     output, torch.Size((1000,)), None, None, 1e-7)*args.multiplier
             # output_t = F.layer_norm(
             #     output_t, torch.Size((1000,)), None, None, 1e-7)*args.multiplier
+            # p = F.softmax(output_t / args.T, dim=-1)
+            # loss_kd = nn.KLDivLoss(reduction="batchmean")(F.log_softmax(output / args.T, dim=1),p
+                                                    #   ) * args.T * args.T * args.alpha
             tea_std = torch.std(output_t, dim=-1,keepdim=True)
             stu_std= torch.std(output, dim=-1, keepdim=True)
             p_s = F.log_softmax(output/stu_std*tea_std/args.T, dim=1)
@@ -412,11 +465,6 @@ def train_kd(train_loader, model, teacher, criterion, optimizer, epoch):
             output = output/stu_std*tea_std
 
         loss_softmax = criterion(output, target_var) * (1-args.alpha)
-
-        p = F.softmax(output_t / args.T, dim=-1)
-
-        loss_kd = nn.KLDivLoss(reduction="batchmean")(F.log_softmax(output / args.T, dim=1),p
-                                                      ) * args.T * args.T * args.alpha
 
         loss = loss_kd+loss_softmax
         prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
